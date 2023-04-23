140b0f5f88fe03f2f21d53c8031823ac96cfb5f4
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-04-23 13:08:41 +0200

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -136,7 +136,6 @@ async def set_skip_number(bot, message):
 
 async def index_files_to_db(lst_msg_id, chat, msg, bot):
     total_files = 0
-    duplicate = 0
     errors = 0
     deleted = 0
     no_media = 0
@@ -175,8 +174,6 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
                 aynav, vnay = await save_file(media)
                 if aynav:
                     total_files += 1
-                elif vnay == 0:
-                    duplicate += 1
                 elif vnay == 2:
                     errors += 1
         except Exception as e:
